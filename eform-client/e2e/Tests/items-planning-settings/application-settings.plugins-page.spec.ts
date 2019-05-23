3be12700824424e8f6326be464d79fd3c53c6716
Yehor <gid733@ya.ru>
2019-05-23 16:50:55 +0300

    Fixed activation

--- eform-client/e2e/Tests/items-planning-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/items-planning-settings/application-settings.plugins-page.spec.ts
@@ -1,53 +1,49 @@
 import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
+import pluginPage from '../../Page objects/Plugin.page';
 
 import {expect} from 'chai';
 import pluginsPage from './application-settings.plugins.page';
-import applicationSettingsPage from '../../Page objects/ApplicationSettings.page';
 
 describe('Application settings page - site header section', function () {
     before(function () {
-      loginPage.open('/auth');
-      loginPage.login();
+        loginPage.open('/auth');
     });
     it('should go to plugin settings page', function () {
-       myEformsPage.Navbar.advancedDropdown();
-       myEformsPage.Navbar.clickonSubMenuItem('Plugins');
-       browser.pause(8000);
+        loginPage.login();
+        myEformsPage.Navbar.advancedDropdown();
+        myEformsPage.Navbar.clickonSubMenuItem('Plugins');
+        browser.pause(8000);
 
-      const plugin = pluginsPage.getFirstPluginRowObj();
-      expect(plugin.id).equal(1);
-      expect(plugin.name).equal('Microting Items Planning Plugin');
-      expect(plugin.version).equal('1.0.0.0');
-      expect(plugin.status, 'Plugin must be deactivated').equal('Deaktiveret');
+        const plugin = pluginsPage.getFirstPluginRowObj();
+        expect(plugin.id).equal(1);
+        expect(plugin.name).equal('Microting Items Planning Plugin');
+        expect(plugin.version).equal('1.0.0.0');
+        expect(plugin.status).equal('Deaktiveret');
+        // expect()
+
     });
+
     it('should activate the plugin', function () {
+        pluginPage.pluginSettingsBtn.click();
+        browser.pause(8000);
+        pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
+        // browser.pause(8000);
+        pluginPage.saveBtn.click();
+        browser.pause(40000);
+        browser.refresh();
+
+        const plugin = pluginsPage.getFirstPluginRowObj();
+        expect(plugin.id).equal(1);
+        expect(plugin.name).equal('Microting Items Planning Plugin');
+        expect(plugin.version).equal('1.0.0.0');
+        expect(plugin.status).equal('Aktiveret');
         // click on plugin settings
-      pluginsPage.goToPluginSettings();
+        // enter connectionstring for customers plugin
         // select activate
-      const menu = pluginsPage.settingsMenu();
-      menu.click();
-      browser.pause(1000);
-      const choices = browser.$$('.ng-option');
-      choices[0].click();
-      const saveBtn = browser.$('#saveBtn');
         // save changes
-      saveBtn.click();
         // see that the plugin is marked active
-      browser.pause(5000);
-      const plugin = pluginsPage.getFirstPluginRowObj();
-      expect(plugin.status, 'Plugin is not active').equal('Aktiveret');
-        // validate that the items planning menu entry is now visible
-      const itemsPlanningMenu = browser.$('#items-planning-pn');
-      const itemsPlanningMenuText = itemsPlanningMenu.getText();
-      expect(itemsPlanningMenuText, 'Items planning not present in header menu').equal( 'Enhedsplanlægning' );
-      itemsPlanningMenu.click();
-      browser.pause(2000);
-      // validate that the lists item is present in plugins menu
-      const itemsPlanningMenuLists = browser.$('#items-planning-pn-lists').getText();
-      expect(itemsPlanningMenuLists, 'Lists not present in items planning menu').equal( 'Lister' );
-      // validate that the lists item is present in plugins menu
-      const itemsPlanningMenuSettings = browser.$('#items-planning-pn-settings').getText();
-      expect(itemsPlanningMenuSettings, 'Settings not present in items planning menu').equal( 'Indstillinger' );
+        // validate that the customers menu entry is now visible
+        // validate that the customers index page is shown with all fields active in the header
     });
 });
