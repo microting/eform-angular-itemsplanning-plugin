11dbb04caabfe34e6054529495cb6c077398389d
René Schultz Madsen <rm@microting.dk>
2019-06-20 11:26:19 +0200

    Adding the missing install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ ! -d "/var/www/microting/eform-angular-itemsplanning-plugin" ]; then
+  cd /var/www/microting
+  su ubuntu -c \
+  "git clone https://github.com/microting/eform-angular-itemsplanning-plugin.git -b stable"
+fi
+
+cd /var/www/microting/eform-angular-itemsplanning-plugin
+su ubuntu -c \
+"dotnet restore eFormAPI/Plugins/ItemsPlanning.Pn/ItemsPlanning.Pn.sln"
+
+echo "################## START GITVERSION ##################"
+export GITVERSION=`git describe --abbrev=0 --tags | cut -d "v" -f 2`
+echo $GITVERSION
+echo "################## END GITVERSION ##################"
+su ubuntu -c \
+"dotnet publish eFormAPI/Plugins/ItemsPlanning.Pn/ItemsPlanning.Pn.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
+
+if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/items-planning-pn"]; then
+	su ubuntu -c \
+	"rm -fR /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/items-planning-pn"
+fi
+
+su ubuntu -c \
+"cp -av /var/www/microting/eform-angular-itemsplanning-plugin/eform-client/src/app/plugins/modules/items-planning-pn /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/items-planning-pn"
+su ubuntu -c \
+"mkdir -p /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/"
+
+if [ -d "/var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection"]; then
+	su ubuntu -c \
+	"rm -fR /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection"
+fi
+
+su ubuntu -c \
+"cp -av /var/www/microting/eform-angular-itemsplanning-plugin/eFormAPI/Plugins/ItemsPlanning.Pn/ItemsPlanning.Pn/out /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection"
+
+
+echo "Recompile angular"
+cd /var/www/microting/eform-angular-frontend/eform-client
+su ubuntu -c \
+"/var/www/microting/eform-angular-itemsplanning-plugin/testinginstallpn.sh"
+su ubuntu -c \
+"npm run build"
+echo "Recompiling angular done"
+
+
