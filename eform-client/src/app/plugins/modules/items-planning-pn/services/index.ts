45ce590543e21850ba7395a63876307910939750
little_crabby <ivan.pangolin@gmail.com>
2019-07-03 17:14:09 +0300

    reports frontend

--- eform-client/src/app/plugins/modules/items-planning-pn/services/index.ts
+++ eform-client/src/app/plugins/modules/items-planning-pn/services/index.ts
@@ -1,2 +1,3 @@
 export * from './items-planning-pn-lists.service';
 export * from './items-planning-pn-settings.service';
+export * from './items-planning-pn-reports.service';
---
+++ eform-client/src/app/plugins/modules/items-planning-pn/services/items-planning-pn-reports.service.ts
@@ -0,0 +1,29 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
+import {OperationDataResult} from 'src/app/common/models/operation.models';
+import {BaseService} from 'src/app/common/services/base.service';
+import {ReportPnFullModel, ReportPnGenerateModel} from '../models/report';
+
+export let ItemsPlanningPnReportsMethods = {
+  Reports: 'api/items-planning-pn/reports',
+};
+
+@Injectable()
+export class ItemsPlanningPnReportsService extends BaseService {
+  constructor(private _http: HttpClient, router: Router, toastrService: ToastrService) {
+    super(_http, router, toastrService);
+  }
+
+  generateReport(model: ReportPnGenerateModel): Observable<OperationDataResult<ReportPnFullModel>> {
+    return this.get(ItemsPlanningPnReportsMethods.Reports, model);
+  }
+
+  getGeneratedReport(model: ReportPnGenerateModel): Observable<OperationDataResult<any>> {
+    return this.getBlobData(ItemsPlanningPnReportsMethods.Reports + '/excel', model);
+  }
+
+}
