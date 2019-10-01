ed006b5c74bc1ca1f8060b645e95cf36e7b8fe18
René Schultz Madsen <rm@microting.dk>
2019-10-01 07:11:44 +0200

    Changing the code a bit to make it pass on travis.

--- eform-client/e2e/Tests/items-planning-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/items-planning-settings/application-settings.plugins-page.spec.ts
@@ -31,8 +31,8 @@ describe('Application settings page - site header section', function () {
         browser.pause(2000);
         browser.refresh();
 
-        browser.waitForVisible('#PluginDropDown', 40000);
-        browser.pause(10000);
+        browser.waitForVisible('#plugin-id', 40000);
+        browser.waitForVisible('Microting Items Planning Plugin', 10000);
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting Items Planning Plugin');
